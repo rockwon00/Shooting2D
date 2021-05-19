c0b1a65aee5db5bbfe0d758d12e1374bce301583
rockwon00 <kojomo80@gmail.com>
2021-05-20 06:39:05 +0900

    .

---
+++ 2d_shooting/Assets/Scripts/gamemanager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class gamemanager : MonoBehaviour
+{
+    public GameObject[] enemyObjs;
+    public Transform[] spawnPoints;
+
+    public float maxSpawnDelay;
+    public float curSpawnDelay;
+
+    void Update()
+    {
+        curSpawnDelay = curSpawnDelay + Time.deltaTime;
+        if (curSpawnDelay > maxSpawnDelay)
+        {
+            SpawnEnemy();
+            maxSpawnDelay = Random.Range(0.5f, 3f);
+            curSpawnDelay = 0;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        int ranEnemy = Random.Range(0, 3);
+        int ranPoint = Random.Range(0, 5);
+        Instantiate(enemyObjs[ranEnemy],
+            spawnPoints[ranPoint].position,
+            spawnPoints[ranPoint].rotation);
+    }
+}
+
